e3dd14e3895ef8fbd61af3611f250b5c8f09108f
gguip1 <51047947+gguip1@users.noreply.github.com>
2025-05-08 15:00:22 +0900

    vite-plugin-pwa 버전 업데이트 및 설치 프롬프트 설정 변경

--- arcana-whiper-front/vite.config.ts
+++ arcana-whiper-front/vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['crystalball-svgrepo-com.svg', 'robots.txt', 'fonts/*.ttf', 'images/*.jpg'],
+      registerType: 'prompt', // 'prompt'로 변경해서 사용자에게 설치 여부를 물어봅니다.
+      includeAssets: ['crystalball-svgrepo-com.svg', 'fonts/*.ttf', 'images/*.jpg'],
       manifest: {
         name: 'Arcana Whisper',
         short_name: 'ArcanaWhisper',
@@ -29,26 +29,26 @@ export default defineConfig({
           }
         ]
       },
+      devOptions: {
+        enabled: true
+      },
       workbox: {
-        // 오프라인 모드에서 작동할 수 있도록 필요한 에셋 캐싱 전략 설정
         globPatterns: ['**/*.{js,css,html,svg,png,jpg,jpeg,ico,json}'],
-        // 네트워크 요청이 실패할 때 오프라인 폴백 페이지 제공
         navigateFallback: '/offline.html',
-        // API 요청에 대한 캐싱 전략
         runtimeCaching: [
           {
-            urlPattern: /^https:\/\/api\.arcanawhisper\.com\/.*$/i, // 실제 API URL에 맞게 수정 필요
+            urlPattern: /^https:\/\/api\.arcanawhisper\.com\/.*$/i,
             handler: 'NetworkFirst',
             options: {
               cacheName: 'api-cache',
               expiration: {
                 maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24, // 24시간
-              },
-            },
-          },
-        ],
-      },
-    }),
+                maxAgeSeconds: 60 * 60 * 24
+              }
+            }
+          }
+        ]
+      }
+    })
   ],
 })
